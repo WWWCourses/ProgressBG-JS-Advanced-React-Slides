5917bc950edf643ee88ae68c7854ab2dae75fb78
WWWCourses <ivawebcourses@gmail.com>
2021-08-18 18:46:36 +0300

    up: Day19

---
+++ pages/themes/TypeScript/examples/slides/ts/test.ts
@@ -0,0 +1,7 @@
+let add = function(x:number,y:number) {
+	console.log(x+y)
+}
+
+add(3,4);
+
+let userName="Ada";
---
+++ pages/themes/TypeScript/examples/slides/ts_demo_project/dist/simpleINterfaceDemo.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+function drawPoint(obj) {
+    console.log(obj.x, obj.y);
+}
+drawPoint({
+    x: 1,
+    y: 2,
+    z: 3
+});
