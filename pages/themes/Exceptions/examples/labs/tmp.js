c3691096a3dbcf72724339ee6dae70f15f391fe7
WWWCourses <ivawebcourses@gmail.com>
2021-11-16 17:36:32 +0200

    gray out future topics

--- pages/themes/Exceptions/examples/labs/tmp.js
+++ pages/themes/Exceptions/examples/labs/tmp.js
@@ -1,10 +1,19 @@
-"use strict";
-function validateEmail(email) {
-    if( email.match(/[\w.-]@\w+\.\w+/) ){
-        return email
-    }else{
-        throw new Error(`invalid email: ${email}`);
+try {
+    console.log(x);
+} catch (e){
+    if (e instanceof EvalError){
+        console.log('EvalError')
+    } else if (e instanceof RangeError){
+        console.log('RangeError')
+    } else if (e instanceof ReferenceError){
+        console.log('ReferenceError')
+    } else if (e instanceof SyntaxError){
+        console.log('SyntaxError')
+    } else if (e instanceof TypeError){
+        console.log('TypeError')
+    } else if (e instanceof URIError){
+        console.log('URIError')
+    } else{
+        console.log('Error!')
     }
 }
-
-var validEmail = validateEmail('name@domaincom');
