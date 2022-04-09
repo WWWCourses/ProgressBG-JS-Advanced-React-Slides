b2e8533541c4311cdd6bc8cb8390ab869232dc96
WWWCourses <ivawebcourses@gmail.com>
2022-04-09 23:27:54 +0300

    update example

--- pages/themes/promisesAndAsync/examples/slides/asyncAwaitBasics.js
+++ pages/themes/promisesAndAsync/examples/slides/asyncAwaitBasics.js
@@ -1,11 +1,13 @@
 async function foo(x) {
+	console.log(`async foo is executed`);
+
 	return x**2
 }
 
 async function add(x) {
 	sum1 = await foo(3)
 	sum2 = await foo(3)
-	console.log(sum1+sum2);
+	console.log(`Total sum: ${sum1+sum2}`);
 
 }
 
@@ -13,7 +15,10 @@ function print(msg) {
 	console.log(msg);
 }
 
+console.log(`1`);
 add(3)
+console.log(`2`);
+
 // foo(3).then(print)
 // console.log(foo(2));
 
