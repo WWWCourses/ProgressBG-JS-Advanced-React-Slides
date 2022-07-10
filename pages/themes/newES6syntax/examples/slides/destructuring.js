a99329b6c405469863d05dd59a995e13da22d5ff
WWWCourses <ivawebcourses@gmail.com>
2022-07-10 17:41:04 +0300

    update

---
+++ pages/themes/newES6syntax/examples/slides/destructuring.js
@@ -0,0 +1,20 @@
+// let a,b;
+// [a,b] = [1,2]
+// console.log(a,b);
+
+
+function greet({name,age}){
+	console.log(`Hello ${name}. You are ${age} years old!`);
+}
+
+const userData = {
+	id: 1,
+	name:'Ada',
+	age: 23,
+}
+
+greet(userData)
+
+
+
+
