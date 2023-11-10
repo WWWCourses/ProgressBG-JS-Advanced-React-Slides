56b267224d5f5828fae4e521934cc58f74d312e8
WWWCourses <ivawebcourses@gmail.com>
2023-11-10 14:50:47 +0200

    add shorter variants

--- pages/themes/mapFilterReduceMethods/HW/groupProductsByCategory.js
+++ pages/themes/mapFilterReduceMethods/HW/groupProductsByCategory.js
@@ -45,34 +45,62 @@ const products = [
 
 
 
-/* ----------------------- Solution with forEach Loop ----------------------- */
-function groupProductsByCategory_ForLoop(products) {
+/* ----------------------- Solution with forEach Loop - Long ----------------------- */
+function groupProductsByCategory_ForEach_Long(products) {
 	let groupedProducts = {};
 
 	products.forEach(p => {
 		if( !groupedProducts[p.category] ){
 			groupedProducts[p.category]=[]
 		}
+
 		groupedProducts[p.category].push(p)
 	});
 
 	return groupedProducts;
 }
 
-/* -------------------------- Solution with Reduce -------------------------- */
-function groupProductsByCategory_Reduce(products) {
-	return products.reduce((groupedProducts, p) => {
-		if( !groupedProducts[p.category] ){
-			groupedProducts[p.category] = []
-		}
-		groupedProducts[p.category].push(p);
+/* -------------------------- Solution with Reduce - Long-------------------------- */
+function groupProductsByCategory_Reduce_Long(products) {
+	return products.reduce((acc, product) => {
+		const category = product.category;
 
+		if( !acc[category] ){
+			acc[category] = []
+		}
+		acc[product.category].push(product);
+		return acc;
+	}, {});
+}
+
+
+/* ----------------------- Solution with forEach Loop - Short----------------------- */
+function groupProductsByCategory_ForEach_Short(products) {
+	let groupedProducts = {};
+
+	products.forEach(p => {
+		// next 2 lines do the same
+		groupedProducts[p.category] = (groupedProducts[p.category] || []).concat(p)
+		// groupedProducts[p.category] = [...(groupedProducts[p.category] || []), p]
+	});
+
+	return groupedProducts;
+}
+
+/* -------------------------- Solution with Reduce - Short-------------------------- */
+function groupProductsByCategory_Reduce_Short(products) {
+	return products.reduce((groupedProducts, p) => {
+		// next 2 lines do the same
+		groupedProducts[p.category] = (groupedProducts[p.category] || []).concat(p)
+		// groupedProducts[p.category] = [...(groupedProducts[p.category] || []), p];
 		return groupedProducts;
 	}, {});
 }
 
 
 // TEST:
-console.log( groupProductsByCategory_ForLoop(products) );
-console.log( groupProductsByCategory_Reduce(products) );
+console.log( groupProductsByCategory_ForEach_Long(products), '\n' );
+console.log( groupProductsByCategory_Reduce_Long(products), '\n' );
+console.log( groupProductsByCategory_ForEach_Short(products), '\n' );
+console.log( groupProductsByCategory_Reduce_Short(products), '\n' );
 
